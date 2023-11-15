27c34682ed9605f7e0a95742dd7777ce8e44ddf0
Vakeeshan Paskaran <99076427+VakeeshanP@users.noreply.github.com>
2023-11-16 00:30:07 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = "Fq7j10IvV8sFO6JavYBZHbCbJA9-6um1XfHmq3iFNkE"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
