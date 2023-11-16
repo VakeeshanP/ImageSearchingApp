694bb7d1d3ff318ac9302ee5ae9284b2d775d49f
Vakeeshan Paskaran <99076427+VakeeshanP@users.noreply.github.com>
2023-11-16 12:05:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,34 @@ const inputEl = document.getElementById("search-input");
 const searchResults = document.querySelector(".search-results");
 const showMore = document.getElementById("show-more-button");
 
-let inputData = "";
 let page = 1;
 
-async function searchImages() {
-    inputData = inputEl.ariaValueMax;
-    const url = `http://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+async function searchImages(query) {
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${accessKey}`;
 
-    const response = await fetch(url);
-    const data = await response.json();
 
-    const results = data.results;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        updateUI(data.results);
+    } catch (error) {
+        console.error('Error fetching images:', error);
+    }
+}
 
+function updateUI(results) {
     if (page === 1) {
         searchResults.innerHTML = "";
     }
 
-    results.map((result) => {
+    results.forEach((result) => {
         const imageWrapper = document.createElement('div');
         imageWrapper.classList.add("search-result");
+
         const image = document.createElement('img');
         image.src = result.urls.small;
         image.alt = result.alt_description;
+
         const imageLink = document.createElement('a');
         imageLink.href = result.links.html;
         imageLink.target = "_blank";
@@ -46,9 +52,9 @@ async function searchImages() {
 formEl.addEventListener("submit", (event) => {
     event.preventDefault();
     page = 1;
-    searchImages();
+    searchImages(inputEl.value);
 });
 
 showMore.addEventListener("click", () => {
-    searchImages();
+    searchImages(inputEl.value);
 });
